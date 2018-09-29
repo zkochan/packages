09839aeefaee2fbe5a28dda2f04273c99b3eccfd
Zoltan Kochan <z@kochan.io>
2018-09-29 22:45:02 +0300

    style(which-pm-runs): fix

--- which-pm-runs/test/index.js
+++ which-pm-runs/test/index.js
@@ -8,19 +8,19 @@ delete process.env.npm_config_user_agent
 const fixturesDir = path.join(__dirname, 'fixtures')
 
 test('detects yarn', t => {
-  execa('yarn', [], {cwd: path.join(fixturesDir, 'yarn')})
+  execa('yarn', [], { cwd: path.join(fixturesDir, 'yarn') })
     .then(() => t.end())
     .catch(t.end)
 })
 
 test('detects npm', t => {
-  execa('npm', ['install'], {cwd: path.join(fixturesDir, 'npm')})
+  execa('npm', ['install'], { cwd: path.join(fixturesDir, 'npm') })
     .then(() => t.end())
     .catch(t.end)
 })
 
 test('detects pnpm', t => {
-  execa('pnpm', ['install'], {cwd: path.join(fixturesDir, 'pnpm')})
+  execa('pnpm', ['install'], { cwd: path.join(fixturesDir, 'pnpm') })
     .then(() => t.end())
     .catch(t.end)
 })
