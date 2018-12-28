16186eecfc7ee65599132904a9b493779543e89e
Zoltan Kochan <z@kochan.io>
2018-12-28 02:47:39 +0200

    test: fix write-ini-file on windows

--- write-ini-file/test.js
+++ write-ini-file/test.js
@@ -1,14 +1,16 @@
-const path = require('path')
+'use strict'
 const fs = require('fs')
-const test = require('tape')
+const path = require('path')
 const tempfile = require('tempfile')
+const test = require('tape')
+const { EOL } = require('os')
 const m = require('write-ini-file')
 
 test('async', t => {
   const tmp = path.join(tempfile(), 'foo')
   m(tmp, { foo: true }, { indent: 2 })
     .then(() => {
-      t.equal(fs.readFileSync(tmp, 'utf8'), 'foo=true\n')
+      t.equal(fs.readFileSync(tmp, 'utf8'), `foo=true${EOL}`)
       t.end()
     })
     .catch(t.end)
@@ -17,6 +19,6 @@ test('async', t => {
 test('sync', t => {
   const tmp = path.join(tempfile(), 'foo')
   m.sync(tmp, { foo: true }, { indent: 2 })
-  t.equal(fs.readFileSync(tmp, 'utf8'), 'foo=true\n')
+  t.equal(fs.readFileSync(tmp, 'utf8'), `foo=true${EOL}`)
   t.end()
 })
