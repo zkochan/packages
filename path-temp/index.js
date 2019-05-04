28f1272d310cfe34f3e4e721a28619e027c986e5
Zoltan Kochan <zoltan.kochan@gmail.com>
2019-05-04 17:35:00 +0300

    feat: update node version in package.json engines

--- path-temp/index.js
+++ path-temp/index.js
@@ -2,6 +2,6 @@
 const path = require('path')
 const uniqueString = require('unique-string')
 
-module.exports = function (folder) {
+module.exports = function pathTemp (folder) {
   return path.join(folder, `_tmp_${process.pid}_${uniqueString()}`)
 }
