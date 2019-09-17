f866fdd7ad51574d5189edc936c61f87aaf1b109
Zoltan Kochan <z@kochan.io>
2019-09-17 07:42:17 +0300

    feat: create help-output

---
+++ help-output/README.md
@@ -0,0 +1,13 @@
+# help-output
+
+> Creates a nice output for a CLI --help
+
+## Installation
+
+```
+<pnpm|yarn|npm> add help-output
+```
+
+## License
+
+[MIT](LICENSE) © [Zoltan Kochan](https://www.kochan.io)
---
+++ help-output/__tests__/index.ts
@@ -0,0 +1,95 @@
+import { stripIndent } from 'common-tags'
+import helpOutput from '../src'
+
+test('single usage', () => {
+    const output = helpOutput({ usages: ['foo [command] [options]'] })
+    expect(output).toBe('Usage: foo [command] [options]')
+})
+
+test('multiple usages', () => {
+    const output = helpOutput({
+        usages: [
+            'foo [command] [options]',
+            'foo --version',
+        ] })
+    expect(output).toBe(stripIndent`
+      Usage: foo [command] [options]
+             foo --version
+    `)
+})
+
+test('single usage with description', () => {
+    const output = helpOutput({
+        description: 'Lorem ipsum.',
+        usages: ['foo [command] [options]'],
+    })
+    expect(output).toBe(stripIndent`
+      Usage: foo [command] [options]
+      
+      Lorem ipsum.
+    `)
+})
+
+test('one alias', () => {
+    const output = helpOutput({ aliases: ['f'], usages: ['foo [command] [options]'] })
+    expect(output).toBe(stripIndent`
+        Usage: foo [command] [options]
+
+        Alias: f
+    `)
+})
+
+test('two aliases', () => {
+    const output = helpOutput({ aliases: ['f', 'fo'], usages: ['foo [command] [options]'] })
+    expect(output).toBe(stripIndent`
+        Usage: foo [command] [options]
+
+        Aliases: f, fo
+    `)
+})
+
+test('description list', () => {
+    const output = helpOutput({
+        descriptionLists: [
+            {
+                title: 'Options',
+                list: [
+                    {
+                         description: 'This forces something',
+                         name: '--force',
+                         shortAlias: '-f',
+                    },
+                    {
+                        description: 'Bar',
+                        name: '--bar'
+                    },
+                    {
+                        name: '--qar'
+                    }
+                ]                
+            },
+            {
+                title: 'Commands',
+                list: [
+                    {
+                        description: 'Foo',
+                        name: 'foo',
+                        shortAlias: 'f'
+                    }
+                ]
+            }
+        ],
+        usages: ['foo [command] [options]'],
+    })
+    expect(output).toBe(stripIndent`
+        Usage: foo [command] [options]
+
+        Options:
+          -f, --force  This forces something
+              --bar    Bar
+              --qar
+        
+        Commands:
+          f, foo  Foo
+    `)
+})
