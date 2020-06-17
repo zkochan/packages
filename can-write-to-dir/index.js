48d36f2b207e3c049f0cb76f3f7b8ba69d386e6e
Zoltan Kochan <z@kochan.io>
2020-06-18 01:19:24 +0300

    feat: create can-write-to-dir

---
+++ can-write-to-dir/index.js
@@ -0,0 +1,37 @@
+'use strict'
+const gracefulFs = require('graceful-fs')
+const pathTemp = require('path-temp')
+
+module.exports = (dir, customFs) => {
+  const fs = customFs || gracefulFs
+  return new Promise((resolve, reject) => {
+    const tempFile = pathTemp(dir)
+    fs.writeFile(tempFile, '', 'utf8', err => {
+      if (!err) {
+        fs.unlink(tempFile, () => {})
+        resolve(true)
+        return
+      }
+      if (err.code === 'EACCES' || err.code === 'EPERM') {
+        resolve(false)
+        return
+      }
+      reject(err)
+    })
+  })
+}
+
+module.exports.sync = (dir, customFs) => {
+  const fs = customFs || gracefulFs
+  const tempFile = pathTemp(dir)
+  try {
+    fs.writeFileSync(tempFile, '', 'utf8')
+    fs.unlinkSync(tempFile)
+    return true
+  } catch (err) {
+    if (err.code === 'EACCES' || err.code === 'EPERM') {
+      return false
+    }
+    throw err
+  }
+}
