d8e326864a9f89d9d299b037af42d637274cf0a6
Zoltan Kochan <z@kochan.io>
2021-03-28 04:11:55 +0300

    feat!(rimraf): use Node's builtin recursive removed

--- rimraf/index.js
+++ rimraf/index.js
@@ -1,3 +1,10 @@
-const { promisify } = require('util')
+const { promises: fs } = require('fs')
 
-module.exports = promisify(require('rimraf'))
+module.exports = async (p) => {
+  try {
+    await fs.rmdir(p, { recursive: true, maxRetries: 3 })
+  } catch (err) {
+    if (err.code === 'ENOTDIR' || err.code === 'ENOENT') return
+    throw err
+  }
+}
