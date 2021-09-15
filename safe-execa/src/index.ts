1a7b2c53260a61c943d0d83541e2bbbbe544b63b
Zoltan Kochan <z@kochan.io>
2021-09-16 01:31:44 +0300

    fix: safe-execa

--- safe-execa/src/index.ts
+++ safe-execa/src/index.ts
@@ -9,27 +9,43 @@ export function	sync (
   args?: readonly string[],
   options?: execa.SyncOptions
 ): execa.ExecaSyncReturnValue {
-  const fileAbsolutePath = getCommandAbsolutePath(file, options)
+  const fileAbsolutePath = getCommandAbsolutePathSync(file, options)
   return execa.sync(fileAbsolutePath, args, options)
 }
 
-export default function (
-  file: string,
-  args?: readonly string[],
-  options?: execa.Options
-): execa.ExecaChildProcess {
-  const fileAbsolutePath = getCommandAbsolutePath(file, options)
-  return execa(fileAbsolutePath, args, options)
-}
-
-function getCommandAbsolutePath (file: string, options?: {
+function getCommandAbsolutePathSync (file: string, options?: {
 		readonly env?: NodeJS.ProcessEnv;
 }) {
   const path = options?.env?.[PATH] ?? process.env[PATH]
   const key = JSON.stringify([path, file])
   let fileAbsolutePath = pathCache.get(key)
   if (fileAbsolutePath == null) {
-    fileAbsolutePath = which(file, { path })
+    fileAbsolutePath = which.sync(file, { path })
+    pathCache.set(key, fileAbsolutePath)
+  }
+  if (fileAbsolutePath == null) {
+    throw new Error(`Couldn't find ${file}`)
+  }
+  return fileAbsolutePath
+}
+
+export default async function (
+  file: string,
+  args?: readonly string[],
+  options?: execa.Options
+) {
+  const fileAbsolutePath = await getCommandAbsolutePath(file, options)
+  return execa(fileAbsolutePath, args, options)
+}
+
+async function getCommandAbsolutePath (file: string, options?: {
+		readonly env?: NodeJS.ProcessEnv;
+}) {
+  const path = options?.env?.[PATH] ?? process.env[PATH]
+  const key = JSON.stringify([path, file])
+  let fileAbsolutePath = pathCache.get(key)
+  if (fileAbsolutePath == null) {
+    fileAbsolutePath = await which(file, { path })
     pathCache.set(key, fileAbsolutePath)
   }
   if (fileAbsolutePath == null) {
