a0597fcb0aaa83a679f12d5a4c14693d297529e1
bluwy <bjornlu.dev@gmail.com>
2024-09-01 00:27:09 +0800

    chore: fix ci

---
+++ test-all.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+const cp = require('child_process')
+const semverSatisfies = require('semver/functions/satisfies')
+
+// Run package tests that only matches the current node version via `engines.node`
+
+const nodeVersion = process.version.slice(1)
+const filteredPackageNames = []
+
+const files = fs.readdirSync(__dirname)
+for (const file of files) {
+  try {
+    const pkg = require(`./${file}/package.json`)
+    if (pkg.engines?.node) {
+      if (semverSatisfies(nodeVersion, pkg.engines.node)) {
+        filteredPackageNames.push(pkg.name)
+      } else {
+        console.log(`Skipping ${pkg.name} because it requires node ${pkg.engines.node}`)
+      }
+    } else {
+      filteredPackageNames.push(pkg.name)
+    }
+  } catch {}
+}
+
+cp.spawnSync(
+  'pnpm',
+  [
+    ...filteredPackageNames.map((name) => `--filter=${name}`),
+    '--no-bail',
+    'test'
+  ],
+  { stdio: 'inherit' }
+)
