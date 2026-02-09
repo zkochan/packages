65f165e94e5089f6addded3d3cf0a4e31d509158
Zoltan Kochan <z@kochan.io>
2026-02-10 00:50:15 +0100

    fix: path-temp across worker threads

--- path-temp/index.js
+++ path-temp/index.js
@@ -1,11 +1,12 @@
 'use strict'
 const path = require('path')
 const uniqueString = require('unique-string')
+const workerThreads = require('node:worker_threads')
 
 module.exports = function pathTemp (folder) {
   return path.join(folder, `_tmp_${process.pid}_${uniqueString()}`)
 }
 
 module.exports.fastPathTemp = function pathTempFast (file) {
-  return path.join(path.dirname(file), `${path.basename(file)}_tmp_${process.pid}`)
+  return path.join(path.dirname(file), `${path.basename(file)}_tmp_${process.pid}_${workerThreads.threadId}`)
 }
